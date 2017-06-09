fc75de458a91a0e9d2637a6cd2354d22c30b3d7d
Matt <matt@mtmc.ca>
2017-06-09 04:54:51 -0600

    dev aws keys removed and deleted

--- aws.php
+++ aws.php
@@ -1,10 +1,10 @@
 <?php
 
 // Replace smtp_username with your Amazon SES SMTP user name.
-define('USERNAME','AKIAIZG4HOGUE2KPNUDA');
+define('USERNAME','FILL IN');
 
 // Replace smtp_password with your Amazon SES SMTP password.
-define('PASSWORD','Am/uZWH5lvXEVNXbMLbGRUglAK9jgOkzEA61QKfrZTAa');
+define('PASSWORD','FILL IN');
 
 // If you're using Amazon SES in a region other than US West (Oregon),
 // replace email-smtp.us-west-2.amazonaws.com with the Amazon SES SMTP
