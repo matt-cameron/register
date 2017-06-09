f1feade6ecaa3d0c3ce36e336d2e9d9c40faf3ea
Matt <matt@mtmc.ca>
2017-06-09 01:13:17 -0600

    added fname to error

--- registration-mailer.php
+++ registration-mailer.php
@@ -9,10 +9,10 @@ define('SENDER', 'matt@mtmc.ca');
 define('RECIPIENT', 'matt@mtmc.ca');
 
 // Replace smtp_username with your Amazon SES SMTP user name.
-define('USERNAME','AKIAIZG4HOGUE2KP');
+define('USERNAME','AKIAIZG4HOGUE2KPNUDA');
 
 // Replace smtp_password with your Amazon SES SMTP password.
-define('PASSWORD','Am/uZWH5lvXEVNXbMLbGRUglAK9jgOkzEA61QKfr');
+define('PASSWORD','Am/uZWH5lvXEVNXbMLbGRUglAK9jgOkzEA61QKfrZTAa');
 
 // If you're using Amazon SES in a region other than US West (Oregon),
 // replace email-smtp.us-west-2.amazonaws.com with the Amazon SES SMTP
@@ -49,6 +49,7 @@ $result = $mail->send(RECIPIENT, $headers, BODY);
 
 if (PEAR::isError($result)) {
   echo("Email not sent. " .$result->getMessage() ."\n");
+  echo $_GET["fname"];
 } else {
   echo("Email sent!"."\n");
 }
